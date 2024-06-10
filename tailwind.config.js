0d49e84159b9dc94e44b8f08f099a16f1d707d03
Mailar <83369389+devmailar@users.noreply.github.com>
2024-06-11 02:02:13 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"radial-gradient": "radial-gradient(circle, #1B1B1B 15%, #000000 100%)",
+			},
+		},
+		fontFamily: {
+			rubik: ["Rubik"],
+		},
+	},
+	plugins: [require("kutty")],
+};

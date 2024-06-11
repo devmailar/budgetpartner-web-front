8b10e4b766994eb25a1048712de9cafa5c4b933c
Mailar <83369389+devmailar@users.noreply.github.com>
2024-06-11 09:53:17 +0300

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import "./index.css";
 const rootElement: HTMLElement | null = document.getElementById("root");
 
 if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+	ReactDOM.createRoot(rootElement).render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+	);
 }

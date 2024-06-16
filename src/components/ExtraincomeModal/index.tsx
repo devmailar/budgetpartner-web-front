fa87388b03bf3f3b6c39473c44f8297b85fd43e1
Mailar <83369389+devmailar@users.noreply.github.com>
2024-06-16 19:24:39 +0300

    Update

--- src/components/ExtraincomeModal/index.tsx
+++ src/components/ExtraincomeModal/index.tsx
@@ -6,53 +6,54 @@ function ExtraincomeModal() {
 
 	return (
 		<Modal index={10}>
-			<form className="flex flex-col gap-y-2 px-6 py-6 w-96">
-				<div className="flex items-center justify-between border-b border-b-[#242424] pb-2">
-					<span className="text-xs text-[#FFFFFF] font-normal font-rubik">
-						Extraincome
-					</span>
-					<span className="text-xs text-[#FFFFFF] font-medium font-rubik">
-						1230.00€
-					</span>
-				</div>
+			<form className="flex flex-col p-4 w-80">
+				<div className="flex flex-col gap-y-3">
+					<div className="flex items-center justify-between">
+						<span className="text-sm text-white font-medium font-rubik">
+							Extraincome 🚀
+						</span>
+						<span className="text-sm text-white font-medium font-rubik">
+							1230.00 €
+						</span>
+					</div>
 
-				<div className="flex flex-col">
-					<button type="button" className="flex items-center justify-between">
-						<span className="text-xs text-[#895FF5] font-thin font-rubik">
-							Oneway Mission
-						</span>
-						<span className="text-xs text-[#895FF5] font-thin font-rubik">
-							1230.00
-						</span>
-					</button>
+					<div className="flex flex-col">
+						<button type="button" className="flex items-center justify-between">
+							<span className="text-xs text-[#895FF5] font-thin font-rubik">
+								Oneway Mission
+							</span>
+							<span className="text-xs text-[#895FF5] font-thin font-rubik">
+								1230.00
+							</span>
+						</button>
 
-					<button type="button" className="flex items-center justify-between">
-						<span className="text-xs text-[#895FF5] font-thin font-rubik">
-							Nordnet
-						</span>
-						<span className="text-xs text-[#895FF5] font-thin font-rubik">
-							150.00
-						</span>
-					</button>
+						<button type="button" className="flex items-center justify-between">
+							<span className="text-xs text-[#895FF5] font-thin font-rubik">
+								Nordnet
+							</span>
+							<span className="text-xs text-[#895FF5] font-thin font-rubik">
+								150.00
+							</span>
+						</button>
+					</div>
 				</div>
 			</form>
 
 			{addExtraincome ? (
-				<form className="flex flex-col gap-y-2 px-4 pt-4 border-t border-t-[#242424] -bg-green-400">
-					<div className="flex gap-x-4 items-center justify-between p-2 border-[1px] border-[#4B4B4B] rounded-lg">
+				<form className="flex flex-col gap-y-2">
+					<div className="flex items-center justify-between mx-4 p-2 border-[1px] border-[#4B4B4B] rounded-lg">
 						<input
-							className="bg-transparent w-full text-sm text-[#FFFFFF] placeholder:text-[#FFFFFF] font-light font-rubik focus:outline-none"
+							className="bg-transparent w-full text-xs text-white placeholder:text-white font-light font-rubik focus:outline-none"
 							type="number"
 							name="income"
 							id="income"
-							placeholder="Description"
 							required
 						/>
 					</div>
 
-					<div className="flex gap-x-4 items-center justify-between p-2 border-[1px] border-[#4B4B4B] rounded-lg">
+					<div className="flex items-center justify-between mx-4 p-2 border-[1px] border-[#4B4B4B] rounded-lg">
 						<input
-							className="bg-transparent w-full text-sm text-[#FFFFFF] placeholder:text-[#FFFFFF] font-light font-rubik focus:outline-none"
+							className="bg-transparent w-full text-xs text-white placeholder:text-white font-light font-rubik focus:outline-none"
 							type="number"
 							name="income"
 							id="income"
@@ -60,12 +61,12 @@ function ExtraincomeModal() {
 							required
 						/>
 
-						<span className="text-xs text-[#FFFFFF] font-light font-rubik">
-							EUR/MONTH
+						<span className="text-xs text-white font-light font-rubik">
+							€/M
 						</span>
 					</div>
 
-					<button type="button" className="btn">
+					<button type="button" className="btn border-t border-t-[#242424]">
 						<div className="flex gap-x-1 items-center justify-center">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -79,7 +80,9 @@ function ExtraincomeModal() {
 									d="M21 7v12q0 .825-.587 1.413T19 21H5q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h12zm-9 11q1.25 0 2.125-.875T15 15t-.875-2.125T12 12t-2.125.875T9 15t.875 2.125T12 18m-6-8h9V6H6z"
 								/>
 							</svg>
-							<span className="text-sm text-[#895FF5] font-normal">Save</span>
+							<span className="text-sm text-[#895FF5] font-normal font-rubik">
+								Save
+							</span>
 						</div>
 					</button>
 				</form>
@@ -89,7 +92,7 @@ function ExtraincomeModal() {
 					className="btn border-t border-t-[#242424]"
 					onClick={(): void => setAddExtraincome(true)}
 				>
-					<div className="flex gap-x-1 items-center justify-center">
+					<div className="flex gap-x-0.5 items-center justify-center">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							width="1.25em"
@@ -102,7 +105,9 @@ function ExtraincomeModal() {
 								d="M11 13H6q-.425 0-.712-.288T5 12t.288-.712T6 11h5V6q0-.425.288-.712T12 5t.713.288T13 6v5h5q.425 0 .713.288T19 12t-.288.713T18 13h-5v5q0 .425-.288.713T12 19t-.712-.288T11 18z"
 							/>
 						</svg>
-						<span className="text-sm text-[#895FF5] font-normal">New</span>
+						<span className="text-sm text-[#895FF5] font-normal font-rubik">
+							New
+						</span>
 					</div>
 				</button>
 			)}
--- src/components/IncomeModalEdit/index.tsx
+++ src/components/IncomeModalEdit/index.tsx
@@ -52,30 +52,46 @@ function IncomeModalEdit() {
 
 	return (
 		<Modal index={10}>
-			<form className="flex flex-col gap-y-6 px-4 py-4" onSubmit={handleSubmit}>
-				<span className="text-sm text-[#FFFFFF] font-medium font-rubik">
-					Set income ✍️
-				</span>
+			<form className="flex flex-col w-80" onSubmit={handleSubmit}>
+				<div className="flex flex-col gap-y-3 p-4 ">
+					<h1 className="text-sm text-white font-medium font-rubik">
+						Set monthly income ✍️
+					</h1>
 
-				<div className="flex gap-x-4 items-center justify-between p-2 border-[1px] border-[#4B4B4B] rounded-lg">
-					<input
-						className="bg-transparent w-full text-sm text-white placeholder:text-white font-light font-rubik focus:outline-none"
-						type="number"
-						name="income"
-						id="income"
-						placeholder={budget.income_amount_monthly.toString()}
-						required
-					/>
+					<div className="flex items-center justify-between p-2 border-[1px] border-[#4B4B4B] rounded-lg">
+						<input
+							className="bg-transparent w-full text-sm text-white placeholder:text-white font-light font-rubik focus:outline-none"
+							type="number"
+							name="income"
+							id="income"
+							placeholder={budget.income_amount_monthly.toString()}
+							required
+						/>
 
-					<span className="text-xs text-white font-light font-rubik">
-						EUR/MONTH
-					</span>
+						<span className="text-xs text-white font-light font-rubik">
+							€/M
+						</span>
+					</div>
 				</div>
 
-				<button type="submit" className="btn bg-[#895FF5] py-2 rounded-lg">
-					<span className="text-xs text-white font-medium font-rubik">
-						Save
-					</span>
+				<button type="submit" className="btn border-t border-t-[#242424]">
+					<div className="flex gap-x-1 items-center justify-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="1.25em"
+							height="1.25em"
+							viewBox="0 0 24 24"
+						>
+							<title>Save</title>
+							<path
+								fill="#895FF5"
+								d="M21 7v12q0 .825-.587 1.413T19 21H5q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h12zm-9 11q1.25 0 2.125-.875T15 15t-.875-2.125T12 12t-2.125.875T9 15t.875 2.125T12 18m-6-8h9V6H6z"
+							/>
+						</svg>
+						<span className="text-sm text-[#895FF5] font-normal font-rubik">
+							Save
+						</span>
+					</div>
 				</button>
 			</form>
 		</Modal>

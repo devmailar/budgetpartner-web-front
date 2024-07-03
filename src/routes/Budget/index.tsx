2498ff531d97ae51d7a55959244c77ad63e85fe6
Mailar <83369389+devmailar@users.noreply.github.com>
2024-07-03 11:41:37 +0300

    Update

--- src/routes/Budget/index.tsx
+++ src/routes/Budget/index.tsx
@@ -17,7 +17,7 @@ import { setBudgets } from "../../stores/Budgets";
 import { setError } from "../../stores/Error";
 import { setModal } from "../../stores/Modal";
 import { setUser } from "../../stores/User";
-import type { IRootState, IUserResponse, TBudget, TExtraincome, TModal } from "../../types";
+import type { IRootState, IUserResponse, TBudget, TExtraexpense, TExtraincome, TModal } from "../../types";
 import { Utils } from "../../utils";
 import "./index.css";
 
@@ -29,6 +29,20 @@ function Budget() {
 	const [monthlyBudgetAmount, setMonthlyBudgetAmount] = React.useState<number>(0);
 
 	const modal: TModal = useSelector((state: IRootState) => state.modal);
+	const months: string[] = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
 
 	const [isLoading, setIsLoading] = React.useState<boolean>(true);
 
@@ -46,7 +60,6 @@ function Budget() {
 				}
 
 				const userResponse: IUserResponse = await response.json();
-				// console.group(userResponse);
 
 				if (Object.keys(userResponse).length === 0) {
 					throw new Error("User response is empty");
@@ -82,15 +95,22 @@ function Budget() {
 					0,
 				);
 
-				console.info(totalExtraincomes);
+				const totalExtraexpenses: number = currentBudget.extraexpenses.reduce(
+					(accumulator: number, extraexpense: TExtraexpense) => {
+						return accumulator + extraexpense.extraexpense_amount_monthly;
+					},
+					0,
+				);
+
+				console.debug({ totalExtraincomes, totalExtraexpenses });
 
 				const currentDaysInMonth: Date[] = eachDayOfInterval({
 					start: startOfMonth(new Date()),
 					end: endOfMonth(new Date()),
 				});
 
-				const dailyBudgetAmount: number = (totalExtraincomes - 0) / currentDaysInMonth.length;
-				const monthlyBudgetAmount: number = totalExtraincomes - 0;
+				const dailyBudgetAmount: number = (totalExtraincomes - totalExtraexpenses) / currentDaysInMonth.length;
+				const monthlyBudgetAmount: number = totalExtraincomes - totalExtraexpenses;
 
 				setDailyBudgetAmount(dailyBudgetAmount);
 				setMonthlyBudgetAmount(monthlyBudgetAmount);
@@ -179,23 +199,26 @@ function Budget() {
 					<div className="loader" />
 				</div>
 			) : (
-				<div className="flex flex-col gap-y-10 items-center mt-14">
-					<div className="flex flex-col items-center">
-						<nav className="flex items-center p-1 bg-[#202020] rounded-full">
-							<button
-								type="button"
-								className="flex items-center justify-center bg-transparent px-10 py-3.5 rounded-full"
-								onClick={(): void => {
-									dispatch(setError("Previous year budget cannot be accessed 🚫"));
-								}}
-							>
-								<span className="text-base text-[#4B4B4B] font-normal font-rubik">{new Date().getFullYear() - 1}</span>
-							</button>
+				<div className="flex flex-col gap-y-10 items-center mt-[5.3rem]">
+					<div className="flex flex-col gap-y-2 items-center">
+						<button
+							type="button"
+							className="flex gap-x-2.5 items-center justify-center bg-[#1A1A1A] px-4 py-2 rounded-[6.25rem]"
+						>
+							<span className="text-base text-[#4B4B4B] font-normal font-rubik">
+								{months[new Date().getMonth()]} {"("}
+								<span className="font-semibold">{new Date().getFullYear()}</span>
+								{")"}
+							</span>
 
-							<button type="button" className="flex items-center justify-center primary px-10 py-3.5 rounded-full">
-								<span className="text-base text-white font-normal font-rubik">{new Date().getFullYear()}</span>
-							</button>
-						</nav>
+							<svg xmlns="http://www.w3.org/2000/svg" width="28" height="27" viewBox="0 0 28 27" fill="none">
+								<title>Switch</title>
+								<path
+									d="M3.03125 13.5C3.03125 19.5592 7.94082 24.4688 14 24.4688C20.0592 24.4688 24.9688 19.5592 24.9688 13.5C24.9688 7.44082 20.0592 2.53125 14 2.53125C7.94082 2.53125 3.03125 7.44082 3.03125 13.5ZM18.2768 11.2113C18.6723 10.8158 19.3209 10.8158 19.7164 11.2113C19.9168 11.4117 20.0117 11.6701 20.0117 11.9285C20.0117 12.1869 19.9115 12.4506 19.7111 12.651L14.7383 17.608C14.3375 17.9719 13.7205 17.9613 13.3355 17.5764L8.28887 12.5455C7.89336 12.15 7.88809 11.5066 8.28887 11.1059C8.68437 10.7104 9.32773 10.7051 9.72852 11.1059L14.0053 15.4248L18.2768 11.2113Z"
+									fill="#895FF5"
+								/>
+							</svg>
+						</button>
 
 						<Swiper
 							className="w-[60rem] h-fit z-0"
@@ -204,7 +227,7 @@ function Budget() {
 							modules={[Autoplay, Pagination]}
 						>
 							<SwiperSlide>
-								<div className="flex flex-col gap-y-2 items-center pt-16 pb-8">
+								<div className="flex flex-col gap-y-2 items-center pt-14 pb-7">
 									<button
 										type="button"
 										onClick={(): void => {
@@ -221,12 +244,12 @@ function Budget() {
 										<h1 className="text-5xl text-[#895FF5] font-bold font-rubik">{dailyBudgetAmount.toFixed(2)} €</h1>
 									</button>
 
-									<span className="text-xl text-[#4B4B4B] font-light font-rubik">/day</span>
+									<span className="text-xl text-[#4B4B4B] font-light font-rubik">day</span>
 								</div>
 							</SwiperSlide>
 
 							<SwiperSlide>
-								<div className="flex flex-col gap-y-2 items-center pt-16 pb-8">
+								<div className="flex flex-col gap-y-2 items-center pt-14 pb-7">
 									<button
 										type="button"
 										onClick={(): void => {
@@ -243,16 +266,16 @@ function Budget() {
 										<h1 className="text-5xl text-[#895FF5] font-bold font-rubik">{monthlyBudgetAmount.toFixed(2)} €</h1>
 									</button>
 
-									<span className="text-xl text-[#4B4B4B] font-light font-rubik">/month</span>
+									<span className="text-xl text-[#4B4B4B] font-light font-rubik">month</span>
 								</div>
 							</SwiperSlide>
 						</Swiper>
 					</div>
 
-					<div className="flex flex-col gap-y-4 items-center">
+					<div className="flex flex-col gap-y-5 items-center">
 						<button
 							type="button"
-							className="flex gap-x-2 items-center justify-center btn bg-[#895FF5] px-4 py-2 regular-income-glow rounded-full"
+							className="flex gap-x-2 items-center justify-center btn bg-[#895FF5] px-[18px] py-2.5 regular-income-glow rounded-[6.25rem]"
 							onClick={(): void => {
 								dispatch(
 									setModal({
@@ -271,7 +294,7 @@ function Budget() {
 
 						<button
 							type="button"
-							className="flex gap-x-2 items-center justify-center btn bg-[#9E553C] px-4 py-2 recurring-expenses-glow rounded-full"
+							className="flex gap-x-2 items-center justify-center btn bg-[#9E553C] px-[18px] py-2.5 recurring-expenses-glow rounded-[6.25rem]"
 							onClick={(): void => {
 								dispatch(
 									setModal({
@@ -285,7 +308,7 @@ function Budget() {
 						>
 							<span className="text-xl text-[#202020] font-medium font-rubik">+</span>
 
-							<span className="text-sm text-[#202020] font-medium font-rubik">Recurring expenses</span>
+							<span className="text-sm text-[#202020] font-medium font-rubik">Extra expenses</span>
 						</button>
 					</div>
 				</div>

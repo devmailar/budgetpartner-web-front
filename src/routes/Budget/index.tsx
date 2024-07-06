11c4163eaa68ca93c49fcf039fbfd4254784a18b
Mailar <83369389+devmailar@users.noreply.github.com>
2024-07-06 11:34:04 +0300

    Button to create new budget in budgetSwitch

--- src/routes/Budget/index.tsx
+++ src/routes/Budget/index.tsx
@@ -182,28 +182,56 @@ function Budget(): React.ReactNode {
 							</button>
 
 							{budgetSwitch && (
-								<div className="absolute z-50 mt-14 flex flex-col gap-y-2 items-center justify-center w-48 py-2 bg-[#1A1A1A] rounded-2xl">
-									{budgets.map((b: TBudget) => (
-										<button
-											key={b.id}
-											type="button"
-											className="px-5 py-0.5 rounded-lg"
-											onClick={() => {
-												setIsLoading(true);
-												setBudgetSwitch(false);
-												dispatch(setBudget(b));
-												setTimeout((): void => setIsLoading(false), 1000);
-											}}
-										>
-											<span
-												className={`text-base ${new Date(budget.created_at).getMonth() === new Date(b.created_at).getMonth() ? "text-white" : "text-[#4B4B4B]"} font-normal font-rubik`}
+								<div className="flex flex-col justify-between absolute z-50 mt-14 w-60 bg-[#1A1A1A] border border-[#202020] rounded-2xl">
+									<div className="flex flex-col gap-y-2 items-center justify-center py-2">
+										{budgets.map((b: TBudget) => (
+											<button
+												key={b.id}
+												type="button"
+												className="px-5 py-0.5 rounded-lg"
+												onClick={() => {
+													setIsLoading(true);
+													setBudgetSwitch(false);
+													dispatch(setBudget(b));
+													setTimeout((): void => setIsLoading(false), 1000);
+												}}
 											>
-												{months[new Date(b.created_at).getMonth()]} {"("}
-												<span className="font-semibold">{new Date(b.created_at).getFullYear()}</span>
-												{")"}
-											</span>
-										</button>
-									))}
+												<span
+													className={`text-base ${new Date(budget.created_at).getMonth() === new Date(b.created_at).getMonth() ? "text-white" : "text-[#4B4B4B]"} font-normal font-rubik`}
+												>
+													{months[new Date(b.created_at).getMonth()]} {"("}
+													<span className="font-semibold">{new Date(b.created_at).getFullYear()}</span>
+													{")"}
+												</span>
+											</button>
+										))}
+									</div>
+
+									<button
+										type="button"
+										className="flex gap-x-2 items-center justify-center border-t border-t-[#202020] py-2.5 rounded-b-2xl"
+									>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="24"
+											height="24"
+											viewBox="0 0 24 24"
+											fill="none"
+											stroke="currentColor"
+											strokeWidth="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											className="text-[#895FF5] icon icon-tabler icons-tabler-outline icon-tabler-hand-grab"
+										>
+											<title>Hand</title>
+											<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+											<path d="M8 11v-3.5a1.5 1.5 0 0 1 3 0v2.5" />
+											<path d="M11 9.5v-3a1.5 1.5 0 0 1 3 0v3.5" />
+											<path d="M14 7.5a1.5 1.5 0 0 1 3 0v2.5" />
+											<path d="M17 9.5a1.5 1.5 0 0 1 3 0v4.5a6 6 0 0 1 -6 6h-2h.208a6 6 0 0 1 -5.012 -2.7l-.196 -.3c-.312 -.479 -1.407 -2.388 -3.286 -5.728a1.5 1.5 0 0 1 .536 -2.022a1.867 1.867 0 0 1 2.28 .28l1.47 1.47" />
+										</svg>
+										<span className="text-base text-[#895FF5] font-normal font-rubik">New Budget</span>
+									</button>
 								</div>
 							)}
 						</div>

0aac619ce385a489daae6f1b738966dab55f836b
Mailar <83369389+devmailar@users.noreply.github.com>
2024-07-14 16:09:51 +0300

    rEMOVE log

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -35,9 +35,8 @@ function Login(): React.ReactNode {
 
 			const authHeader: string = loginUserResponse.headers.get("Authorization") ?? "";
 			const auth: string = authHeader.split(" ")[1];
-			console.log("🚀 ~ handleLogin ~ auth:", auth);
 
-			setCookie("Authorization", auth, { expires: 1 });
+			setCookie("Authorization", auth, { expires: 1, sameSite: "strict", secure: true });
 
 			navigate("/budget");
 		} catch (error: unknown) {

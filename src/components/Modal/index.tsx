29db83d36bcffd5011568991e67004891bd3156e
Mailar <83369389+devmailar@users.noreply.github.com>
2024-07-15 22:03:23 +0300

    Hajoaaa

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -8,7 +8,7 @@ function Modal({ index, children }: { index: number; children: React.ReactNode }
 
 	return (
 		<div
-			className={`absolute z-${index} flex items-center justify-center w-screen h-screen bg-black bg-opacity-20`}
+			className={`absolute z-${index} flex items-center justify-center w-screen h-screen top-0 bg-black bg-opacity-20`}
 			onClick={(): void => {
 				dispatch(
 					setModal({

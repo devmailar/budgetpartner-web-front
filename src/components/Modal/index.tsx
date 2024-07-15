c9e551120bd0c4fb9eff71a509f04f44344b8ca4
Mailar <83369389+devmailar@users.noreply.github.com>
2024-07-15 21:45:56 +0300

    Add zoomed in to modals

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,13 +1,14 @@
 import type { Dispatch } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import { setModal } from "../../stores/Modal";
+import "./index.css";
 
 function Modal({ index, children }: { index: number; children: React.ReactNode }) {
 	const dispatch: Dispatch = useDispatch();
 
 	return (
 		<div
-			className={`absolute z-${index} flex items-start justify-center w-screen h-screen bg-black bg-opacity-20`}
+			className={`absolute z-${index} flex items-start justify-center w-screen h-screen bg-black bg-opacity-20 zoomed-in`}
 			onClick={(): void => {
 				dispatch(
 					setModal({

1def84809b53ffa2803f772e93e48086859ab6b4
Mailar <83369389+devmailar@users.noreply.github.com>
2024-07-19 17:59:41 +0300

    Update sizes

--- src/components/SettingsModal/index.tsx
+++ src/components/SettingsModal/index.tsx
@@ -46,7 +46,7 @@ function SettingsModal(): React.ReactNode {
 	};
 
 	return (
-		<Modal index={40} classes="gap-y-3 px-6 py-6 animate__animated animate__fadeInDown animate__faster">
+		<Modal index={40} classes="gap-y-2 px-6 py-6 w-[28rem] animate__animated animate__fadeInDown animate__faster">
 			<div className="flex items-center justify-between">
 				<span className="text-base text-white font-medium font-rubik">Settings</span>
 
@@ -66,8 +66,8 @@ function SettingsModal(): React.ReactNode {
 				</button>
 			</div>
 
-			<div className="flex gap-x-4 justify-between pt-4 border-t border-t-grey">
-				<div className="flex flex-col gap-y-6 pr-4 border-r border-r-grey">
+			<div className="flex gap-x-4 items-center pt-2 border-t border-t-dark">
+				<div className="flex flex-col gap-y-6 pr-4 border-r border-r-dark">
 					<div className="flex flex-col gap-y-3">
 						<button type="button" className="flex gap-x-2 items-center w-fit">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 40 40" fill="none">
@@ -225,46 +225,56 @@ function SettingsModal(): React.ReactNode {
 					</div>
 				</div>
 
-				<div className="flex flex-col gap-y-2">
+				<div className="flex flex-col gap-y-8">
 					<div className="flex flex-col gap-y-2">
-						<span className="text-sm text-white font-normal font-rubik">Email</span>
+						<div className="flex flex-col gap-y-1">
+							<span className="text-sm text-white font-normal font-rubik">Email</span>
 
-						<div className="flex items-center justify-between">
-							<span className="text-sm text-white font-normal font-rubik">
-								{hideEmail ? "··········" : `${user.email}`}
-							</span>
+							<div className="flex items-center justify-between">
+								<span className="text-sm text-white font-normal font-rubik">
+									{hideEmail ? "··········" : `${user.email}`}
+								</span>
 
-							<button type="button" onClick={(): void => setHideEmail(!hideEmail)}>
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-									<title>Eye</title>
-									<g clip-path="url(#clip0_284_119)">
-										<path
-											d="M10 12C10 12.5304 10.2107 13.0391 10.5858 13.4142C10.9609 13.7893 11.4696 14 12 14C12.5304 14 13.0391 13.7893 13.4142 13.4142C13.7893 13.0391 14 12.5304 14 12C14 11.4696 13.7893 10.9609 13.4142 10.5858C13.0391 10.2107 12.5304 10 12 10C11.4696 10 10.9609 10.2107 10.5858 10.5858C10.2107 10.9609 10 11.4696 10 12Z"
-											stroke="white"
-											stroke-width="2"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-										/>
-										<path
-											d="M21 12C18.6 16 15.6 18 12 18C8.4 18 5.4 16 3 12C5.4 8 8.4 6 12 6C15.6 6 18.6 8 21 12Z"
-											stroke="white"
-											stroke-width="2"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-										/>
-									</g>
-									<defs>
-										<clipPath id="clip0_284_119">
-											<rect width="24" height="24" fill="white" />
-										</clipPath>
-									</defs>
-								</svg>
+								<button type="button" onClick={(): void => setHideEmail(!hideEmail)}>
+									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+										<title>Eye</title>
+										<g clip-path="url(#clip0_284_119)">
+											<path
+												d="M10 12C10 12.5304 10.2107 13.0391 10.5858 13.4142C10.9609 13.7893 11.4696 14 12 14C12.5304 14 13.0391 13.7893 13.4142 13.4142C13.7893 13.0391 14 12.5304 14 12C14 11.4696 13.7893 10.9609 13.4142 10.5858C13.0391 10.2107 12.5304 10 12 10C11.4696 10 10.9609 10.2107 10.5858 10.5858C10.2107 10.9609 10 11.4696 10 12Z"
+												stroke="white"
+												stroke-width="2"
+												stroke-linecap="round"
+												stroke-linejoin="round"
+											/>
+											<path
+												d="M21 12C18.6 16 15.6 18 12 18C8.4 18 5.4 16 3 12C5.4 8 8.4 6 12 6C15.6 6 18.6 8 21 12Z"
+												stroke="white"
+												stroke-width="2"
+												stroke-linecap="round"
+												stroke-linejoin="round"
+											/>
+										</g>
+										<defs>
+											<clipPath id="clip0_284_119">
+												<rect width="24" height="24" fill="white" />
+											</clipPath>
+										</defs>
+									</svg>
+								</button>
+							</div>
+						</div>
+
+						<div className="flex flex-col gap-y-1">
+							<span className="text-sm text-white font-normal font-rubik">Password</span>
+
+							<button type="button" className="btn bg-grey px-2 py-1 w-fit">
+								<span className="text-xs text-light font-medium font-rubik">Change password</span>
 							</button>
 						</div>
 					</div>
 
-					<button type="button" className="btn bg-red">
-						<span className="text-xs">Delete your account</span>
+					<button type="button" className="btn bg-red px-2 py-1 w-fit">
+						<span className="text-xs font-medium font-rubik">Delete account</span>
 					</button>
 				</div>
 			</div>

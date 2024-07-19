39b8d47969f3a6118be539b45703e14b683d5b4f
Mailar <83369389+devmailar@users.noreply.github.com>
2024-07-20 02:23:51 +0300

    Nii

--- src/components/ExtraexpenseModal/index.tsx
+++ src/components/ExtraexpenseModal/index.tsx
@@ -131,159 +131,143 @@ function ExtraexpenseModal(): React.ReactNode {
 	}, [navigate]);
 
 	return (
-		<Modal index={40}>
-			<div className="animate__animated animate__fadeInDown animate__faster min-w-80 px-2.5 py-2.5 bg-dark border border-darker rounded-2xl">
-				<div className="flex flex-col">
-					{!removeExtraexpenseModal && (
-						<div className="flex items-center justify-end">
-							<button type="button" onClick={(): void => handleClose()}>
-								<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-									<title>Close</title>
-									<g clipPath="url(#clip0_245_208)">
-										<path
-											d="M15 5L5 15"
-											stroke="#4B4B4B"
-											strokeWidth="2"
-											strokeLinecap="round"
-											strokeLinejoin="round"
-										/>
-										<path
-											d="M5 5L15 15"
-											stroke="#4B4B4B"
-											strokeWidth="2"
-											strokeLinecap="round"
-											strokeLinejoin="round"
-										/>
-									</g>
-									<defs>
-										<clipPath id="clip0_245_208">
-											<rect width="20" height="20" fill="white" />
-										</clipPath>
-									</defs>
-								</svg>
-							</button>
+		<Modal index={40} classes="gap-y-2 px-2.5 py-2.5 min-w-80 animate__animated animate__fadeInDown animate__faster">
+			{!removeExtraexpenseModal && (
+				<div className="flex items-center justify-end">
+					<button type="button" onClick={(): void => handleClose()}>
+						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+							<title>Close</title>
+							<g clipPath="url(#clip0_245_208)">
+								<path d="M15 5L5 15" stroke="#4B4B4B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+								<path d="M5 5L15 15" stroke="#4B4B4B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+							</g>
+							<defs>
+								<clipPath id="clip0_245_208">
+									<rect width="20" height="20" fill="white" />
+								</clipPath>
+							</defs>
+						</svg>
+					</button>
+				</div>
+			)}
+
+			{!createExtraexpenseModal && !removeExtraexpenseModal ? (
+				<div className="flex flex-col gap-4 px-7 py-2">
+					<div className="flex gap-x-3 items-center justify-between">
+						<div className="flex gap-x-1 items-center">
+							<span className="text-base text-white font-medium font-rubik">Total expenses</span>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="28"
+								height="28"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="#B85C3D"
+								strokeWidth="2"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							>
+								<title>Trending Down</title>
+								<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+								<path d="M3 7l6 6l4 -4l8 8" />
+								<path d="M21 10l0 7l-7 0" />
+							</svg>
 						</div>
-					)}
 
-					{!createExtraexpenseModal && !removeExtraexpenseModal ? (
-						<div className="flex flex-col gap-4 px-7 py-2">
-							<div className="flex gap-x-3 items-center justify-between">
-								<div className="flex gap-x-1 items-center">
-									<span className="text-base text-white font-medium font-rubik">Total expenses</span>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										width="28"
-										height="28"
-										viewBox="0 0 24 24"
-										fill="none"
-										stroke="#B85C3D"
-										strokeWidth="2"
-										strokeLinecap="round"
-										strokeLinejoin="round"
-									>
-										<title>Trending Down</title>
-										<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-										<path d="M3 7l6 6l4 -4l8 8" />
-										<path d="M21 10l0 7l-7 0" />
-									</svg>
-								</div>
+						<span className="text-base text-white font-medium font-rubik">{totalExtraexpenses.toFixed(1)}€</span>
+					</div>
 
-								<span className="text-base text-white font-medium font-rubik">{totalExtraexpenses.toFixed(1)}€</span>
-							</div>
+					<div className="flex flex-col items-start">
+						{budget.extraexpenses.map((extraexpense: IExtraexpense) => (
+							<button
+								key={extraexpense.extraexpense_type}
+								type="button"
+								onClick={(): void => {
+									setRemoveExtraexpense(extraexpense);
+									setRemoveExtraexpenseModal(true);
+								}}
+							>
+								<span className="text-base text-white font-normal font-rubik">
+									{extraexpense.extraexpense_type}: {extraexpense.extraexpense_amount_monthly.toFixed(1)}€
+								</span>
+							</button>
+						))}
+					</div>
 
-							<div className="flex flex-col items-start">
-								{budget.extraexpenses.map((extraexpense: IExtraexpense) => (
-									<button
-										key={extraexpense.extraexpense_type}
-										type="button"
-										onClick={(): void => {
-											setRemoveExtraexpense(extraexpense);
-											setRemoveExtraexpenseModal(true);
-										}}
-									>
-										<span className="text-base text-white font-normal font-rubik">
-											{extraexpense.extraexpense_type}: {extraexpense.extraexpense_amount_monthly.toFixed(1)}€
-										</span>
-									</button>
-								))}
-							</div>
+					<button
+						type="button"
+						className="btn bg-transparent px-3 py-2 rounded-xl"
+						onClick={(): void => setCreateExtraexpenseModal(true)}
+					>
+						<span className="text-base text-orange font-medium font-rubik">Create</span>
+					</button>
+				</div>
+			) : createExtraexpenseModal ? (
+				<form className="flex flex-col gap-y-4 px-4" onSubmit={handleCreateExtraexpense}>
+					<span className="text-base text-white font-medium font-rubik">Create new expense 🚀</span>
+
+					<div className="flex flex-col gap-y-3">
+						<div className="flex items-center justify-between p-2.5 border-[0.5px] border-[#4B4B4B] rounded-lg">
+							<input
+								className="bg-transparent text-sm text-white placeholder:text-light font-normal font-rubik focus:outline-none"
+								type="text"
+								name="extraexpense_type"
+								id="extraexpense_type"
+								placeholder="Source"
+								required
+							/>
+						</div>
+
+						<div className="flex items-center justify-between p-2.5 border-[0.5px] border-[#4B4B4B] rounded-lg">
+							<input
+								className="bg-transparent text-sm text-white placeholder:text-light font-normal font-rubik focus:outline-none"
+								type="number"
+								name="extraexpense_amount_monthly"
+								id="extraexpense_amount_monthly"
+								placeholder="0.00"
+								required
+							/>
+
+							<span className="text-sm text-light font-normal font-rubik">€/MO</span>
+						</div>
+					</div>
+
+					<button type="submit" className="btn bg-transparent px-3 py-2 rounded-xl">
+						<span className="text-base text-orange font-medium font-rubik">Save</span>
+					</button>
+				</form>
+			) : (
+				removeExtraexpenseModal && (
+					<div className="animate__animated animate__fadeIn flex flex-col gap-y-4 px-4 items-center justify-center max-w-80">
+						<span className="text-base text-center text-white font-medium font-rubik">Warning</span>
+
+						<span className="text-sm text-center text-light font-light font-rubik">
+							This operation is permanent and will delete expense{" "}
+							<code>
+								{removeExtraexpense.extraexpense_type}: {removeExtraexpense.extraexpense_amount_monthly.toFixed(1)}€
+							</code>
+						</span>
+
+						<div className="flex gap-2 items-center">
+							<button
+								type="submit"
+								className="btn bg-transparent px-3 py-2 rounded-xl"
+								onClick={(): Promise<void> => handleRemoveExtraexpense(removeExtraexpense.id)}
+							>
+								<span className="text-base text-red font-medium font-rubik">Delete</span>
+							</button>
 
 							<button
-								type="button"
+								type="submit"
 								className="btn bg-transparent px-3 py-2 rounded-xl"
-								onClick={(): void => setCreateExtraexpenseModal(true)}
+								onClick={(): void => setRemoveExtraexpenseModal(false)}
 							>
-								<span className="text-base text-orange font-medium font-rubik">Create</span>
+								<span className="text-base text-light font-medium font-rubik">Cancel</span>
 							</button>
 						</div>
-					) : createExtraexpenseModal ? (
-						<form className="flex flex-col gap-y-4 px-4" onSubmit={handleCreateExtraexpense}>
-							<span className="text-base text-white font-medium font-rubik">Create new expense 🚀</span>
-
-							<div className="flex flex-col gap-y-3">
-								<div className="flex items-center justify-between p-2.5 border-[0.5px] border-[#4B4B4B] rounded-lg">
-									<input
-										className="bg-transparent text-sm text-white placeholder:text-light font-normal font-rubik focus:outline-none"
-										type="text"
-										name="extraexpense_type"
-										id="extraexpense_type"
-										placeholder="Source"
-										required
-									/>
-								</div>
-
-								<div className="flex items-center justify-between p-2.5 border-[0.5px] border-[#4B4B4B] rounded-lg">
-									<input
-										className="bg-transparent text-sm text-white placeholder:text-light font-normal font-rubik focus:outline-none"
-										type="number"
-										name="extraexpense_amount_monthly"
-										id="extraexpense_amount_monthly"
-										placeholder="0.00"
-										required
-									/>
-
-									<span className="text-sm text-light font-normal font-rubik">€/MO</span>
-								</div>
-							</div>
-
-							<button type="submit" className="btn bg-transparent px-3 py-2 rounded-xl">
-								<span className="text-base text-orange font-medium font-rubik">Save</span>
-							</button>
-						</form>
-					) : (
-						removeExtraexpenseModal && (
-							<div className="animate__animated animate__fadeIn flex flex-col gap-y-4 px-4 items-center justify-center max-w-80">
-								<span className="text-base text-center text-white font-medium font-rubik">Warning</span>
-
-								<span className="text-sm text-center text-light font-light font-rubik">
-									This operation is permanent and will delete expense{" "}
-									<code>
-										{removeExtraexpense.extraexpense_type}: {removeExtraexpense.extraexpense_amount_monthly.toFixed(1)}€
-									</code>
-								</span>
-
-								<div className="flex gap-2 items-center">
-									<button
-										type="submit"
-										className="btn bg-transparent px-3 py-2 rounded-xl"
-										onClick={(): Promise<void> => handleRemoveExtraexpense(removeExtraexpense.id)}
-									>
-										<span className="text-base text-red font-medium font-rubik">Delete</span>
-									</button>
-
-									<button
-										type="submit"
-										className="btn bg-transparent px-3 py-2 rounded-xl"
-										onClick={(): void => setRemoveExtraexpenseModal(false)}
-									>
-										<span className="text-base text-light font-medium font-rubik">Cancel</span>
-									</button>
-								</div>
-							</div>
-						)
-					)}
-				</div>
-			</div>
+					</div>
+				)
+			)}
 		</Modal>
 	);
 }
--- src/components/ExtraincomeModal/index.tsx
+++ src/components/ExtraincomeModal/index.tsx
@@ -131,159 +131,143 @@ function ExtraincomeModal(): React.ReactNode {
 	}, [navigate]);
 
 	return (
-		<Modal index={40}>
-			<div className="animate__animated animate__fadeInDown animate__faster min-w-80 px-2.5 py-2.5 bg-dark border border-darker rounded-2xl">
-				<div className="flex flex-col">
-					{!removeExtraincomeModal && (
-						<div className="flex items-center justify-end">
-							<button type="button" onClick={(): void => handleClose()}>
-								<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-									<title>Close</title>
-									<g clipPath="url(#clip0_245_208)">
-										<path
-											d="M15 5L5 15"
-											stroke="#4B4B4B"
-											strokeWidth="2"
-											strokeLinecap="round"
-											strokeLinejoin="round"
-										/>
-										<path
-											d="M5 5L15 15"
-											stroke="#4B4B4B"
-											strokeWidth="2"
-											strokeLinecap="round"
-											strokeLinejoin="round"
-										/>
-									</g>
-									<defs>
-										<clipPath id="clip0_245_208">
-											<rect width="20" height="20" fill="white" />
-										</clipPath>
-									</defs>
-								</svg>
-							</button>
+		<Modal index={40} classes="gap-y-2 px-2.5 py-2.5 min-w-80 animate__animated animate__fadeInDown animate__faster">
+			{!removeExtraincomeModal && (
+				<div className="flex items-center justify-end">
+					<button type="button" onClick={(): void => handleClose()}>
+						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+							<title>Close</title>
+							<g clipPath="url(#clip0_245_208)">
+								<path d="M15 5L5 15" stroke="#4B4B4B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+								<path d="M5 5L15 15" stroke="#4B4B4B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+							</g>
+							<defs>
+								<clipPath id="clip0_245_208">
+									<rect width="20" height="20" fill="white" />
+								</clipPath>
+							</defs>
+						</svg>
+					</button>
+				</div>
+			)}
+
+			{!createExtraincomeModal && !removeExtraincomeModal ? (
+				<div className="flex flex-col gap-4 px-7 py-2">
+					<div className="flex gap-x-3 items-center justify-between">
+						<div className="flex gap-x-1 items-center">
+							<span className="text-base text-white font-medium font-rubik">Total income</span>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="28"
+								height="28"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="#895FF5"
+								strokeWidth="2"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							>
+								<title>Trending Up</title>
+								<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+								<path d="M3 17l6 -6l4 4l8 -8" />
+								<path d="M14 7l7 0l0 7" />
+							</svg>
 						</div>
-					)}
 
-					{!createExtraincomeModal && !removeExtraincomeModal ? (
-						<div className="flex flex-col gap-4 px-7 py-2">
-							<div className="flex gap-x-3 items-center justify-between">
-								<div className="flex gap-x-1 items-center">
-									<span className="text-base text-white font-medium font-rubik">Total income</span>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										width="28"
-										height="28"
-										viewBox="0 0 24 24"
-										fill="none"
-										stroke="#895FF5"
-										strokeWidth="2"
-										strokeLinecap="round"
-										strokeLinejoin="round"
-									>
-										<title>Trending Up</title>
-										<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-										<path d="M3 17l6 -6l4 4l8 -8" />
-										<path d="M14 7l7 0l0 7" />
-									</svg>
-								</div>
+						<span className="text-base text-white font-medium font-rubik">{totalExtraincomes.toFixed(1)}€</span>
+					</div>
 
-								<span className="text-base text-white font-medium font-rubik">{totalExtraincomes.toFixed(1)}€</span>
-							</div>
+					<div className="flex flex-col items-start">
+						{budget.extraincomes.map((extraincome: IExtraincome) => (
+							<button
+								key={extraincome.extraincome_type}
+								type="button"
+								onClick={(): void => {
+									setRemoveExtraincome(extraincome);
+									setRemoveExtraincomeModal(true);
+								}}
+							>
+								<span className="text-base text-white font-normal font-rubik">
+									{extraincome.extraincome_type}: {extraincome.extraincome_amount_monthly.toFixed(1)}€
+								</span>
+							</button>
+						))}
+					</div>
 
-							<div className="flex flex-col items-start">
-								{budget.extraincomes.map((extraincome: IExtraincome) => (
-									<button
-										key={extraincome.extraincome_type}
-										type="button"
-										onClick={(): void => {
-											setRemoveExtraincome(extraincome);
-											setRemoveExtraincomeModal(true);
-										}}
-									>
-										<span className="text-base text-white font-normal font-rubik">
-											{extraincome.extraincome_type}: {extraincome.extraincome_amount_monthly.toFixed(1)}€
-										</span>
-									</button>
-								))}
-							</div>
+					<button
+						type="button"
+						className="btn bg-transparent px-3 py-2 rounded-xl"
+						onClick={(): void => setCreateExtraincomeModal(true)}
+					>
+						<span className="text-base text-purple font-medium font-rubik">Create</span>
+					</button>
+				</div>
+			) : createExtraincomeModal ? (
+				<form className="flex flex-col gap-y-4 px-4" onSubmit={handleCreateExtraincome}>
+					<span className="text-base text-white font-medium font-rubik">Create new Income 🚀</span>
+
+					<div className="flex flex-col gap-y-3">
+						<div className="flex items-center justify-between p-2.5 border-[0.5px] border-[#4B4B4B] rounded-lg">
+							<input
+								className="bg-transparent text-sm text-white placeholder:text-light font-normal font-rubik focus:outline-none"
+								type="text"
+								name="extraincome_type"
+								id="extraincome_type"
+								placeholder="Source"
+								required
+							/>
+						</div>
+
+						<div className="flex items-center justify-between p-2.5 border-[0.5px] border-[#4B4B4B] rounded-lg">
+							<input
+								className="bg-transparent text-sm text-white placeholder:text-light font-normal font-rubik focus:outline-none"
+								type="number"
+								name="extraincome_amount_monthly"
+								id="extraincome_amount_monthly"
+								placeholder="0.00"
+								required
+							/>
+
+							<span className="text-sm text-light font-normal font-rubik">€/MO</span>
+						</div>
+					</div>
+
+					<button type="submit" className="btn bg-transparent px-3 py-2 rounded-xl">
+						<span className="text-base text-purple font-medium font-rubik">Save</span>
+					</button>
+				</form>
+			) : (
+				removeExtraincomeModal && (
+					<div className="animate__animated animate__fadeIn flex flex-col gap-y-4 px-4 items-center justify-center max-w-80">
+						<span className="text-base text-center text-white font-medium font-rubik">Warning</span>
+
+						<span className="text-sm text-center text-light font-light font-rubik">
+							This operation is permanent and will delete income{" "}
+							<code>
+								{removeExtraincome.extraincome_type}: {removeExtraincome.extraincome_amount_monthly.toFixed(1)}€
+							</code>
+						</span>
+
+						<div className="flex gap-2 items-center">
+							<button
+								type="submit"
+								className="btn bg-transparent px-3 py-2 rounded-xl"
+								onClick={(): Promise<void> => handleRemoveExtraincome(removeExtraincome.id)}
+							>
+								<span className="text-base text-red font-medium font-rubik">Delete</span>
+							</button>
 
 							<button
-								type="button"
+								type="submit"
 								className="btn bg-transparent px-3 py-2 rounded-xl"
-								onClick={(): void => setCreateExtraincomeModal(true)}
+								onClick={(): void => setRemoveExtraincomeModal(false)}
 							>
-								<span className="text-base text-purple font-medium font-rubik">Create</span>
+								<span className="text-base text-light font-medium font-rubik">Cancel</span>
 							</button>
 						</div>
-					) : createExtraincomeModal ? (
-						<form className="flex flex-col gap-y-4 px-4" onSubmit={handleCreateExtraincome}>
-							<span className="text-base text-white font-medium font-rubik">Create new Income 🚀</span>
-
-							<div className="flex flex-col gap-y-3">
-								<div className="flex items-center justify-between p-2.5 border-[0.5px] border-[#4B4B4B] rounded-lg">
-									<input
-										className="bg-transparent text-sm text-white placeholder:text-light font-normal font-rubik focus:outline-none"
-										type="text"
-										name="extraincome_type"
-										id="extraincome_type"
-										placeholder="Source"
-										required
-									/>
-								</div>
-
-								<div className="flex items-center justify-between p-2.5 border-[0.5px] border-[#4B4B4B] rounded-lg">
-									<input
-										className="bg-transparent text-sm text-white placeholder:text-light font-normal font-rubik focus:outline-none"
-										type="number"
-										name="extraincome_amount_monthly"
-										id="extraincome_amount_monthly"
-										placeholder="0.00"
-										required
-									/>
-
-									<span className="text-sm text-light font-normal font-rubik">€/MO</span>
-								</div>
-							</div>
-
-							<button type="submit" className="btn bg-transparent px-3 py-2 rounded-xl">
-								<span className="text-base text-purple font-medium font-rubik">Save</span>
-							</button>
-						</form>
-					) : (
-						removeExtraincomeModal && (
-							<div className="animate__animated animate__fadeIn flex flex-col gap-y-4 px-4 items-center justify-center max-w-80">
-								<span className="text-base text-center text-white font-medium font-rubik">Warning</span>
-
-								<span className="text-sm text-center text-light font-light font-rubik">
-									This operation is permanent and will delete income{" "}
-									<code>
-										{removeExtraincome.extraincome_type}: {removeExtraincome.extraincome_amount_monthly.toFixed(1)}€
-									</code>
-								</span>
-
-								<div className="flex gap-2 items-center">
-									<button
-										type="submit"
-										className="btn bg-transparent px-3 py-2 rounded-xl"
-										onClick={(): Promise<void> => handleRemoveExtraincome(removeExtraincome.id)}
-									>
-										<span className="text-base text-red font-medium font-rubik">Delete</span>
-									</button>
-
-									<button
-										type="submit"
-										className="btn bg-transparent px-3 py-2 rounded-xl"
-										onClick={(): void => setRemoveExtraincomeModal(false)}
-									>
-										<span className="text-base text-light font-medium font-rubik">Cancel</span>
-									</button>
-								</div>
-							</div>
-						)
-					)}
-				</div>
-			</div>
+					</div>
+				)
+			)}
 		</Modal>
 	);
 }

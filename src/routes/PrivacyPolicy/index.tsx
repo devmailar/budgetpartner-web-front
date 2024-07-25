d8772a7870de120e37b5ea06cd20c4d1813f3337
Mailar <83369389+devmailar@users.noreply.github.com>
2024-07-25 10:09:26 +0300

    C,pn

--- src/routes/PrivacyPolicy/index.tsx
+++ src/routes/PrivacyPolicy/index.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 
 function PrivacyPolicy(): React.ReactNode {
 	return (
-		<div className="flex items-center justify-center bg-radial-gradient w-screen h-screen text-white">
+		<div className="flex flex-col items-center justify-center bg-radial-gradient w-screen h-screen text-white">
 			<div>
 				<h2>Privacy Policy</h2>
 				<p>

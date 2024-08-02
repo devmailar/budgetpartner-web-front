9bd13237590cc51eda7aa312dcad8b8b566a86fc
Mailar <83369389+devmailar@users.noreply.github.com>
2024-08-02 19:45:15 +0300

    🆗🫡

--- src/components/ExtraincomeModal/index.tsx
+++ src/components/ExtraincomeModal/index.tsx
@@ -296,16 +296,16 @@ function ExtraincomeModal(): React.ReactNode {
 													setRemoveExtraincomeModal(true);
 												}}
 											>
-												<td className="px-0 py-2 text-left text-sm text-light font-medium font-rubik truncate">
+												<td className="px-0 py-2 text-left text-sm text-light font-normal font-rubik truncate">
 													<span>{index + 1}</span>
 												</td>
-												<td className="px-3 py-2 text-left text-sm text-light font-medium font-rubik truncate">
+												<td className="px-3 py-2 text-left text-sm text-light font-normal font-rubik truncate">
 													<span>{extraincome.extraincome_type}</span>
 												</td>
-												<td className="px-3 py-2 text-left text-sm text-light font-medium font-rubik truncate">
+												<td className="px-3 py-2 text-left text-sm text-light font-normal font-rubik truncate">
 													<span>{extraincome.extraincome_amount_monthly.toFixed(1)}€</span>
 												</td>
-												<td className="px-0 py-2 text-right text-sm text-light font-medium font-rubik truncate">
+												<td className="px-0 py-2 text-right text-sm text-light font-normal font-rubik truncate">
 													<span>{new Date(extraincome.created_at).toLocaleDateString()}</span>
 												</td>
 											</tr>

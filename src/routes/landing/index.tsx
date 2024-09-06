f50d189085cb1bd9778fb167c7f4dccb67a90c87
Mailar <83369389+devmailar@users.noreply.github.com>
2024-09-06 13:33:11 +0300

    Update Landing page

--- src/routes/landing/index.tsx
+++ src/routes/landing/index.tsx
@@ -3,8 +3,8 @@ import type React from "react";
 
 function Landing(): React.ReactNode {
 	return (
-		<div className="flex items-center justify-center h-screen animate__faster animate__animated animate__fadeIn animate__delay-1s">
-			<h1 className="text-8xl text-white text-center font-medium">
+		<div className="flex flex-wrap items-center justify-center h-screen py-10 px-10 animate__faster animate__animated animate__fadeIn animate__delay-1s">
+			<h1 className="text-6xl md:text-8xl text-white text-center font-medium">
 				BudgetPartner <br /> simplifies <br /> budgeting
 			</h1>
 

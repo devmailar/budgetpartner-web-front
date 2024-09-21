f158fc74b6fc6d2e97d6c0ac9dd1bd4358e895a5
Mailar <83369389+devmailar@users.noreply.github.com>
2024-09-21 09:13:21 +0300

    UMM..

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,6 @@ import Dexie, { type EntityTable } from "dexie";
 
 export interface BudgetTable {
 	id: number;
-	user_id: number;
 	currency: string;
 
 	created_at: Date;
@@ -13,10 +12,10 @@ export interface ExtraincomeTable {
 	id: number;
 	budget_id: number;
 
-	extraincome_type: string;
-	extraincome_amount_monthly: number;
-	extraincome_includes_weekends: boolean;
-	extraincome_date: Date;
+	type: string;
+	amountMonthly: number;
+	includesWeekends: boolean;
+	date: Date;
 
 	created_at: Date;
 	updated_at: Date;
@@ -26,9 +25,9 @@ export interface ExtraexpenseTable {
 	id: number;
 	budget_id: number;
 
-	extraexpense_type: string;
-	extraexpense_amount_monthly: number;
-	extraexpense_date: Date;
+	type: string;
+	amountMonthly: number;
+	date: Date;
 
 	created_at: Date;
 	updated_at: Date;
@@ -41,9 +40,7 @@ export const db: Dexie = new Dexie("Database") as Dexie & {
 };
 
 db.version(1).stores({
-	budgets: "++id, user_id, currency, created_at, updated_at",
-	extraincomes:
-		"++id, budget_id, extraincome_type, extraincome_amount_monthly, extraincome_includes_weekends, extraincome_date, created_at, updated_at",
-	extraexpenses:
-		"++id, budget_id, extraexpense_type, extraexpense_amount_monthly, extraexpense_date, created_at, updated_at",
+	budgets: "++id, currency, created_at, updated_at",
+	extraincomes: "++id, budget_id, type, amountMonthly, includesWeekends, date, created_at, updated_at",
+	extraexpenses: "++id, budget_id, type, amountMonthly, date, created_at, updated_at",
 });

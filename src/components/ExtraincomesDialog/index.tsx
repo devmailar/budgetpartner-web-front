2c898a3c6b14daa0e559211baed3b2807ea86f4d
Mailar <83369389+devmailar@users.noreply.github.com>
2024-09-28 19:59:45 +0300

    lets test on phone

--- src/components/ExtraincomesDialog/index.tsx
+++ src/components/ExtraincomesDialog/index.tsx
@@ -28,7 +28,7 @@ function ExtraincomesDialog(): React.ReactNode {
 	});
 
 	return (
-		<div className="animate__animated animate__slideInUp animate__faster flex flex-col gap-4 absolute bottom-0 bg-black w-full h-[64%] md:h-[90%] px-7 pb-5 border border-[#212121] rounded-3xl">
+		<div className="animate__animated animate__slideInUp animate__faster flex flex-col gap-y-6 absolute bottom-0 bg-black w-full h-[64%] md:h-[90%] px-7 pb-5 border border-[#212121] rounded-3xl">
 			<button
 				type="button"
 				className="flex items-center justify-center pt-[18px]"
@@ -55,96 +55,119 @@ function ExtraincomesDialog(): React.ReactNode {
 				/>
 			</button>
 
-			<div className="flex items-center justify-between">
-				<span className="text-base text-[#007AFF] font-semibold">Total Income</span>
-				<span className="text-lg text-white font-bold">
-					{totalExtraincomes ? totalExtraincomes.toFixed(2) : "···"}
-					{Utils.formatCurrencyFunction(budgetStore.currency)}
-				</span>
+			<div className="flex gap-x-3 items-center justify-end h-[26px]">
+				<button
+					type="button"
+					className="bg-[#007AFF] h-full px-2 py-0 rounded-2xl"
+					onClick={(): void => {
+						navigate("/new-extraincome");
+					}}
+				>
+					<span className="text-base text-white font-medium">+ Add new</span>
+				</button>
 
-				<div className="flex">
-					<button
-						type="button"
-						className="btn bg-transparent"
-						onClick={() => {
-							navigate("/new-extraincome");
-						}}
-					>
-						<span className="text-base text-[#007AFF] font-semibold">+ Add new</span>
-					</button>
-				</div>
+				<button
+					type="button"
+					className="bg-[#1B1818] h-full px-6 py-0 rounded-2xl"
+					onClick={(): void => {
+						dispatch(
+							setDialogStore({
+								extraincomes: false,
+								extraexpenses: false,
+							}),
+						);
+					}}
+				>
+					<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<title>Close</title>
+						<path
+							d="M14 1.16049L13.1045 0L7 5.97531L0.895476 0L0 1.16049L5.96256 7L0 12.8395L0.895476 14L7 8.02469L13.1045 14L14 12.8395L8.03744 7L14 1.16049Z"
+							fill="white"
+						/>
+					</svg>
+				</button>
 			</div>
 
-			<div className="flex flex-col gap-y-6 h-72 md:h-[40rem] overflow-scroll">
-				{extraincomesSortedByCreatedAtAscending.length > 0 &&
-					extraincomesSortedByCreatedAtAscending.map((extraincome: IExtraincome, index: number) => (
-						<button
-							type="button"
-							key={extraincome.id}
-							className="flex flex-col gap-y-1 btn px-0 py-0 w-full rounded-none"
-							onClick={async (): Promise<void> => {
-								if (
-									confirm(
-										`Are you sure you want to remove income "${extraincome.type}" with amount ${extraincome.amount_monthly.toFixed(2)}${Utils.formatCurrencyFunction(budgetStore.currency)}?`,
-									)
-								) {
-									const removeExtraincomeResponse: Response = await fetch(
-										`${Utils.baseUrl}/extraincomes/remove/${extraincome.id}`,
-										{
-											method: "DELETE",
+			<div className="flex flex-col gap-y-3">
+				<div className="flex items-center justify-between">
+					<span className="text-2xl text-white font-semibold">Total Income</span>
+					<span className="text-2xl text-white font-bold">
+						{totalExtraincomes ? totalExtraincomes.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, " ") : "···"}
+						{Utils.formatCurrencyFunction(budgetStore.currency)}
+					</span>
+				</div>
+
+				<div className="flex flex-col gap-y-6 h-72 md:h-[40rem] overflow-scroll">
+					{extraincomesSortedByCreatedAtAscending.length > 0 &&
+						extraincomesSortedByCreatedAtAscending.map((extraincome: IExtraincome, index: number) => (
+							<button
+								type="button"
+								key={extraincome.id}
+								className="flex flex-col gap-y-1 btn px-0 py-0 w-full rounded-none"
+								onClick={async (): Promise<void> => {
+									if (
+										confirm(
+											`Are you sure you want to remove income "${extraincome.type}" with amount ${extraincome.amount_monthly.toFixed(2)}${Utils.formatCurrencyFunction(budgetStore.currency)}?`,
+										)
+									) {
+										const removeExtraincomeResponse: Response = await fetch(
+											`${Utils.baseUrl}/extraincomes/remove/${extraincome.id}`,
+											{
+												method: "DELETE",
+												headers: { Authorization: `Bearer ${authStore}` },
+											},
+										);
+
+										if (!removeExtraincomeResponse.ok) {
+											const removeExtraincomeResponseError: IResponseError = await removeExtraincomeResponse.json();
+
+											throw new Error(removeExtraincomeResponseError.errorMessage);
+										}
+
+										const getUserResponse: Response = await fetch(`${Utils.baseUrl}/users/get`, {
+											method: "GET",
 											headers: { Authorization: `Bearer ${authStore}` },
-										},
-									);
+										});
 
-									if (!removeExtraincomeResponse.ok) {
-										const removeExtraincomeResponseError: IResponseError = await removeExtraincomeResponse.json();
+										if (!getUserResponse.ok) {
+											const getUserResponseError: IResponseError = await getUserResponse.json();
 
-										throw new Error(removeExtraincomeResponseError.errorMessage);
+											throw new Error(getUserResponseError.errorMessage);
+										}
+
+										const getUserResponseBody: IUserResponse = await getUserResponse.json();
+
+										dispatch(setUserStore(getUserResponseBody.errorNoData.user));
+										dispatch(setBudgetsStore(getUserResponseBody.errorNoData.budgets));
+
+										const currentBudget: IBudget | undefined = getUserResponseBody.errorNoData.budgets.find(
+											(budget: IBudget): boolean => {
+												return new Date(budget.created_at).getMonth() === new Date().getMonth();
+											},
+										);
+
+										dispatch(setBudgetStore(currentBudget));
 									}
+								}}
+							>
+								<div className="flex items-center gap-x-2 w-full">
+									<span className="text-xl text-[#66666F] font-normal">
+										{new Date(extraincome.date).toDateString()}
+									</span>
 
-									const getUserResponse: Response = await fetch(`${Utils.baseUrl}/users/get`, {
-										method: "GET",
-										headers: { Authorization: `Bearer ${authStore}` },
-									});
+									<hr className="flex-grow bg-[#66666F] text-[#66666F] h-[0.5px] border-none" />
+								</div>
 
-									if (!getUserResponse.ok) {
-										const getUserResponseError: IResponseError = await getUserResponse.json();
-
-										throw new Error(getUserResponseError.errorMessage);
-									}
-
-									const getUserResponseBody: IUserResponse = await getUserResponse.json();
-
-									dispatch(setUserStore(getUserResponseBody.errorNoData.user));
-									dispatch(setBudgetsStore(getUserResponseBody.errorNoData.budgets));
-
-									const currentBudget: IBudget | undefined = getUserResponseBody.errorNoData.budgets.find(
-										(budget: IBudget): boolean => {
-											return new Date(budget.created_at).getMonth() === new Date().getMonth();
-										},
-									);
-
-									dispatch(setBudgetStore(currentBudget));
-								}
-							}}
-						>
-							<div className="flex items-center gap-x-2 w-full">
-								<span className="text-base text-[#66666F] font-medium">
-									{new Date(extraincome.date).toDateString()}
-								</span>
-
-								<hr className="flex-grow bg-[#66666F] text-[#66666F] h-[0.5px] border-none" />
-							</div>
-
-							<div className="flex items-center justify-between w-full">
-								<span className="text-xl text-[#91919A] font-medium truncate">{extraincome.type}</span>
-								<span className="text-xl text-[#007AFF] font-medium truncate">
-									+{extraincome.amount_monthly.toFixed(2)}
-									{Utils.formatCurrencyFunction(budgetStore.currency)}
-								</span>
-							</div>
-						</button>
-					))}
+								<div className="flex items-center justify-between w-full">
+									<span className="text-xl text-[#91919A] font-medium truncate">{extraincome.type}</span>
+									<span className="text-xl text-[#007AFF] font-medium truncate">
+										+{extraincome.amount_monthly.toFixed(2)}
+										{Utils.formatCurrencyFunction(budgetStore.currency)}
+									</span>
+								</div>
+							</button>
+						))}
+				</div>
 			</div>
 		</div>
 	);

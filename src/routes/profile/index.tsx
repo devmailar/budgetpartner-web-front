9de518beea1f206d9a817fe7452bb741fab6f05c
Mailar <83369389+devmailar@users.noreply.github.com>
2024-10-19 10:28:59 +0300

    Update

--- src/routes/profile/index.tsx
+++ src/routes/profile/index.tsx
@@ -56,28 +56,14 @@ const Profile = (): ReactNode => {
 	};
 
 	return (
-		<div className="flex flex-col gap-y-8 animate__animated animate__slideInRight animate__faster">
-			<nav className="flex items-center justify-between px-8 pt-4">
-				<a href="/" className="text-lg text-white font-bold">
-					BudgetPartner
-				</a>
-
-				<button type="button" className="btn px-0.5 py-0.5" onClick={(): void => navigate("/")}>
-					<svg width="34" height="34" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<title>Arrow Back</title>
-						<path d="M9 14L5 10L9 6" stroke="#007AFF" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" />
-						<path
-							d="M5 10H16C17.0609 10 18.0783 10.4214 18.8284 11.1716C19.5786 11.9217 20 12.9391 20 14C20 15.0609 19.5786 16.0783 18.8284 16.8284C18.0783 17.5786 17.0609 18 16 18H15"
-							stroke="#007AFF"
-							strokeWidth="2"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						/>
-					</svg>
+		<div className="flex flex-col gap-y-12 animate__animated animate__slideInRight animate__faster">
+			<nav className="flex items-center justify-start px-8 pt-3">
+				<button type="button" onClick={(): void => navigate("/")}>
+					<span className="text-xl text-[#66666F] font-bold">Back</span>
 				</button>
 			</nav>
 
-			<div className="flex flex-col gap-y-6 items-start px-8">
+			<div className="flex flex-col gap-y-6 px-8">
 				<button type="button" onClick={(): Promise<void> => handleDelete()}>
 					<span className="text-lg text-[#66666F] font-normal underline">Delete Account</span>
 				</button>

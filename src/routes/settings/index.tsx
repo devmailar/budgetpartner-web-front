3ce675183b5fbddeeaeec61a9812848d1fae65b2
Mailar <83369389+devmailar@users.noreply.github.com>
2024-10-19 10:32:38 +0300

    AB

--- src/routes/settings/index.tsx
+++ src/routes/settings/index.tsx
@@ -12,28 +12,22 @@ const Settings = (): ReactNode => {
 	const budgetStore: IBudgetState["value"] = useBudgetStore.getState().value;
 
 	return (
-		<div className="h-screen animate__animated animate__slideInRight animate__faster">
-			<nav className="flex items-center justify-between px-5 py-2.5 border-b border-b-[#313131]">
-				<h2 className="text-lg text-white font-medium">BudgetPartner</h2>
-
+		<div className="flex flex-col gap-y-12 animate__animated animate__slideInRight animate__faster">
+			<nav className="flex items-center justify-start px-8 pt-3">
 				<button type="button" onClick={(): void => navigate("/")}>
-					<span className="text-lg text-[#007AFF] font-medium">Back</span>
+					<span className="text-xl text-[#66666F] font-bold">Back</span>
 				</button>
 			</nav>
 
-			<div className="flex flex-col gap-y-6 items-center justify-center px-6 py-6">
-				<h2 className="text-2xl font-semibold text-white">Settings</h2>
-
+			<div className="flex flex-col gap-y-6 px-8">
 				<div className="flex flex-col gap-y-1 items-center">
-					<span className="font-base font-semibold text-[#007AFF]">Currency</span>
+					<span className="font-lg text-[#66666F] font-semibold">Currency</span>
 					<select
 						name="currencies"
 						className="px-2.5 py-1.5 bg-transparent border-[1.5px] border-[#3F3F46] rounded-2xl text-lg text-[#66666F] font-normal"
 						defaultValue={budgetStore.currency}
 						onChange={async (e: ChangeEvent<HTMLSelectElement>) => {
 							try {
-								// update {budgetStore.id} currency column in table budgets
-
 								const changeCurrencyResponse: Response = await fetch(`${Utils.baseUrl}/budgets/change-currency`, {
 									method: "POST",
 									headers: { Authorization: `Bearer ${authStore}`, "Content-Type": "application/json" },
@@ -45,8 +39,6 @@ const Settings = (): ReactNode => {
 
 									throw new Error(changeCurrencyResponseError.errorMessage);
 								}
-
-								// fetch the user again and set budgetStore state
 							} catch (error: unknown) {
 								if (error instanceof Error) {
 									alert(error.message);

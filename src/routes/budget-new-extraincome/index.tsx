2fcf5de1745ad8350b929479c497138b238ad740
Mailar <83369389+devmailar@users.noreply.github.com>
2024-10-19 10:19:50 +0300

    ABB

--- src/routes/budget-new-extraincome/index.tsx
+++ src/routes/budget-new-extraincome/index.tsx
@@ -105,7 +105,7 @@ const BudgetNewExtraincome = (): ReactNode => {
 			<form className="flex flex-col gap-y-6 items-center px-8" onSubmit={handleSubmit}>
 				<h2 className="text-2xl text-white font-bold">New Income</h2>
 
-				<div className="flex flex-col gap-y-2">
+				<div className="flex flex-col gap-y-2 w-full">
 					<div className="flex gap-x-2 items-center px-4 py-3 bg-[#18181B] border border-[#212121] rounded-lg">
 						<svg width={24} height={24} fill="none" viewBox="0 0 24 24">
 							<title>Description</title>

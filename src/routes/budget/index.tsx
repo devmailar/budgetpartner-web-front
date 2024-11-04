a241df9ffa4248d75ad283c047181d7c92d0caa8
Mailar <83369389+devmailar@users.noreply.github.com>
2024-11-04 08:28:41 +0200

    Testing haptic feedback

--- src/routes/budget/index.tsx
+++ src/routes/budget/index.tsx
@@ -1,3 +1,4 @@
+import { Haptics } from "@capacitor/haptics";
 import React, { type ReactNode, useEffect, useState } from "react";
 import { type NavigateFunction, useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
@@ -435,7 +436,8 @@ const Budget = (): ReactNode => {
 							<button
 								type="button"
 								className={`flex items-center justify-center w-full btn px-2 py-1 ${selectedTab === "incomes" ? "bg-[#4400DE] animate__animated animate__pulse animate__slower animate__infinite" : "bg-transparent"} rounded-full`}
-								onClick={(): void => {
+								onClick={async (): Promise<void> => {
+									await Haptics.vibrate();
 									setSelectedTab("incomes");
 									localStorage.setItem("selectedTab", "incomes");
 								}}
@@ -451,7 +453,8 @@ const Budget = (): ReactNode => {
 							<button
 								type="button"
 								className={`flex items-center justify-center w-full btn px-2 py-1 ${selectedTab === "expenses" ? "bg-[#4400DE] animate__animated animate__pulse animate__slower animate__infinite" : "bg-transparent"} rounded-full`}
-								onClick={(): void => {
+								onClick={async (): Promise<void> => {
+									await Haptics.vibrate();
 									setSelectedTab("expenses");
 									localStorage.setItem("selectedTab", "expenses");
 								}}
